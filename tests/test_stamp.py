3597dfa89622b678fa9af31f20a5d9096075463d
Fábio Madeira <fbiomadeira@gmail.com>
2017-08-28 17:53:47 +0100

    Added new methods for reading writting STAMP domains. Also added new tests. More to come.

--- tests/test_stamp.py
+++ tests/test_stamp.py
@@ -16,7 +16,14 @@ try:
 except ImportError:
     from unittest.mock import patch
 
-from prointvar.stamp import parse_stamp_scan_scores_from_file
+from prointvar.sifts import SIFTSreader, get_sifts_selected_from_table
+from prointvar.pdbx import PDBXreader, PDBXwriter, get_mmcif_selected_from_table
+from prointvar.merger import TableMerger
+
+from prointvar.stamp import (parse_stamp_scan_scores_from_file,
+                             get_stamp_domain_line,
+                             parse_stamp_domain_definitions_from_from_file,
+                             write_stamp_domain_definitions_from_table)
 
 from prointvar.config import config as c
 
@@ -31,22 +38,103 @@ class TestSTAMP(unittest.TestCase):
     def setUp(self):
         """Initialize the framework for testing."""
 
+        self.pdb_id = TestSTAMP.pdb_id
+        self.chain_id = TestSTAMP.chain_id
+        self.domains_info = TestSTAMP.domains_info
+
         self.input_scan = os.path.join(c.db_root, c.db_stamp, "scan.scores")
+        self.domainfile = os.path.join(c.db_root, c.db_stamp, "domain.file")
+
         self.excluded = ()
 
         self.parse_stamp_scan_scores_from_file = parse_stamp_scan_scores_from_file
+        self.get_stamp_domain_line = get_stamp_domain_line
+        self.parse_stamp_domain_definitions = parse_stamp_domain_definitions_from_from_file
+        self.write_stamp_domain_definitions = write_stamp_domain_definitions_from_table
 
         logging.disable(logging.DEBUG)
 
     def tearDown(self):
         """Remove testing framework."""
 
+        self.pdb_id = None
+        self.chain_id = None
+        self.domains_info = None
+
         self.input_scan = None
+        self.domainfile = None
+
         self.excluded = None
 
         self.parse_stamp_scan_scores_from_file = None
+        self.get_stamp_domain_line = None
+        self.parse_stamp_domain_definitions = None
+        self.write_stamp_domain_definitions = None
 
         logging.disable(logging.NOTSET)
+
+    @classmethod
+    def setUpClass(cls):
+        # to be run only once
+        super(TestSTAMP, cls).setUpClass()
+
+        cls.pdb_id = "2pah"
+        cls.chain_id = "A"
+        cls.domains_info = []
+        inputsifts = os.path.join(c.db_root, c.db_sifts, "%s.xml" % cls.pdb_id)
+        s = SIFTSreader(inputfile=inputsifts)
+        r = s.regions()
+        if 'Pfam' in r[cls.chain_id]:
+            for reg in r[cls.chain_id]['Pfam']:
+                # UniProt Coordinates
+                start = r[cls.chain_id]['Pfam'][reg]['start']
+                end = r[cls.chain_id]['Pfam'][reg]['end']
+
+                inputcif = os.path.join(c.db_root, c.db_pdbx, "%s.cif" % cls.pdb_id)
+                p = PDBXreader(inputfile=inputcif)
+                cif_table = p.atoms(category='auth', format_type="mmcif")
+                cif_table = get_mmcif_selected_from_table(cif_table, chain=(cls.chain_id,),
+                                                          category='auth',
+                                                          lines=("ATOM",))
+                inputsifts = os.path.join(c.db_root, c.db_sifts, "%s.xml" % cls.pdb_id)
+                s = SIFTSreader(inputfile=inputsifts)
+                r = s.regions()
+                sifts_table = s.residues()
+                sifts_table = get_sifts_selected_from_table(sifts_table,
+                                                            chain_auth=(cls.chain_id,))
+
+                t = TableMerger(pdbx_table=cif_table, sifts_table=sifts_table, store=False)
+                table = t.merge()
+                uni_sites = tuple([str(i) for i in range(start, end)])
+                table = get_sifts_selected_from_table(table, chain_auth=(cls.chain_id,),
+                                                      site=uni_sites)
+
+                outputpdb = os.path.join(c.db_root, c.db_pdbx, "%s_%s_%s.pdb" % (cls.pdb_id,
+                                                                                 cls.chain_id,
+                                                                                 reg))
+                p = PDBXwriter(outputfile=outputpdb)
+                p.run(data=cif_table, format_type="pdb")
+
+                # TODO loop over residues for getting the actual res ranges in the table
+                # this is needed for STAMP
+                start = table.loc[list(table.index)[0], "auth_seq_id"]
+                start_inscode = table.loc[list(table.index)[0], "pdbx_PDB_ins_code"]
+                end = table.loc[list(table.index)[-1], "auth_seq_id"]
+                end_inscode = table.loc[list(table.index)[-1], "pdbx_PDB_ins_code"]
+                info = {'pdb_id': cls.pdb_id, 'chain_id': cls.chain_id,
+                        'start': (start,), 'end': (end,),
+                        'start_inscode': (start_inscode,),
+                        'end_inscode': (end_inscode,),
+                        'path': outputpdb,
+                        'domain_id': '%s_%s_%s' % (cls.pdb_id, cls.chain_id, reg)}
+                cls.domains_info.append(info)
+
+    @classmethod
+    def tearDownClass(cls):
+
+        cls.pdb_id = None
+        cls.chain_id = None
+        cls.domains_info = None
 
     def test_parse_stamp_scan_scores_from_file(self):
         table = self.parse_stamp_scan_scores_from_file(self.input_scan,
@@ -61,6 +149,12 @@ class TestSTAMP(unittest.TestCase):
         self.assertEqual(0.084, table.loc[0, 'RMS'])
         self.assertEqual(280, table.loc[0, 'A_Len'])
         self.assertEqual(98.57, table.loc[0, 'PID'])
+
+    def test_write_stamp_domain_definitions(self):
+        self.write_stamp_domain_definitions(self.domainfile,
+                                            self.domains_info,
+                                            override=True)
+        self.assertTrue(os.path.isfile(self.domainfile))
 
 
 if __name__ == '__main__':
