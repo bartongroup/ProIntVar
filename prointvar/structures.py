7ba030abf01bbb48d8132846052705c002ef2deb
Fábio Madeira <fbiomadeira@gmail.com>
2017-11-23 17:39:34 +0000

    Further removed bits from structures that I have now added to ProteoFAV 0.2.2.

--- prointvar/structures.py
+++ prointvar/structures.py
@@ -7,135 +7,12 @@ Extends the functionality of ProteoFAV.structures
 Fábio Madeira, 2017+
 """
 
-import os
-import shlex
 import logging
-import numpy as np
-import pandas as pd
-from scipy.spatial import cKDTree
-from collections import OrderedDict
 
 from prointvar.utils import get_new_pro_ids
 
 
 logger = logging.getLogger("prointvar")
-
-
-def _tokenize(handle):
-    for line in handle:
-        if line.startswith("#"):
-            continue
-        elif line.startswith(";"):
-            token = line[1:].strip()
-            for line in handle:
-                line = line.strip()
-                if line == ';':
-                    break
-                token += line
-            yield token
-        else:
-            tokens = shlex.split(line)
-            for token in tokens:
-                yield token
-
-
-def parse_mmcif_categories_from_file(inputfile, excluded=(), category=None):
-    """
-    Generic method that gets all categories and fields (except for the .atom_site*)
-
-    :param inputfile: path to the mmCIF file
-    :param excluded: option to exclude mmCIF categories
-    :param category: data category to be parsed (otherwise all)
-    :return: returns a nested dictionary
-    """
-
-    logger.info("Parsing mmCIF categories from lines...")
-
-    if not os.path.isfile(inputfile):
-        raise IOError("{} not available or could not be read...".format(inputfile))
-
-    full_dict = OrderedDict()
-    with open(inputfile) as handle:
-        loop_flag = False
-        key = None
-        full_tokens = _tokenize(handle)
-        full_token = next(full_tokens)
-        full_dict[full_token[0:5]] = full_token[5:]
-        i = 0
-        n = 0
-        for full_token in full_tokens:
-            if full_token == "loop_":
-                loop_flag = True
-                keys = []
-                i = 0
-                n = 0
-                continue
-            elif loop_flag:
-                if full_token.startswith("_"):
-                    if i > 0:
-                        loop_flag = False
-                    else:
-                        full_dict[full_token] = []
-                        keys.append(full_token)
-                        n += 1
-                        continue
-                else:
-                    full_dict[keys[i % n]].append(full_token)
-                    i += 1
-                    continue
-            if key is None:
-                key = full_token
-            else:
-                full_dict[key] = [full_token]
-                key = None
-
-    mmcid_object = OrderedDict()
-    for full_key in full_dict:
-        if full_key != "data_":
-            main_key = full_key.split(".")[0]
-            lead_key = full_key.split(".")[1]
-            if (category is not None and main_key == category) or category is None:
-                if main_key not in excluded:
-                    if main_key not in mmcid_object:
-                        mmcid_object[main_key] = {}
-                    mmcid_object[main_key][lead_key] = full_dict[full_key]
-
-    return mmcid_object
-
-
-def get_contact_indexes_from_table(data, dist=5):
-    """
-    Gets the DataFrame indexes of the nearby atoms under a provided radius (dist).
-
-    :param data: pandas DataFrame object
-    :param dist: distance threshold in Angstrom
-    :return: list of pandas DataFrame indexes
-    """
-
-    table = data
-    tree = cKDTree(table[['Cartn_x', 'Cartn_y', 'Cartn_z']])
-    query_point = table.loc[:, ['Cartn_x', 'Cartn_y', 'Cartn_z']]
-    indexes = tree.query_ball_point(query_point, r=dist)
-
-    return indexes
-
-
-def add_mmcif_contacts(data, dist=5):
-    """
-    Utility that adds a new column to the table.
-    Adds a new column with the contacting indexes (str)
-
-    :param data: pandas DataFrame object
-    :return: returns a modified pandas DataFrame
-    """
-
-    table = data
-    indexes = get_contact_indexes_from_table(table, dist=dist)
-    assert len(indexes) == len(table)
-    nindexes = [','.join([str(z) for z in x]) for x in indexes]
-    table['contact_indexes'] = nindexes
-
-    return table
 
 
 def add_mmcif_new_pro_ids(data, category='auth'):
@@ -176,23 +53,4 @@ def add_mmcif_new_pro_ids(data, category='auth'):
         raise StopIteration(message)
     return table
 
-
-def get_coordinates(table):
-    """
-    Get coordinates from the provided PDBx table
-    and return a vector-set with all the coordinates.
-
-    :param table: pandas DataFrame from PDBXreader
-    :returns: coordinates - array (N,3) where N is number of atoms
-    """
-
-    # assumes it's a valid PDBx table
-    if isinstance(table, pd.DataFrame):
-        coords = [np.array([table.loc[i, "Cartn_x"],
-                            table.loc[i, "Cartn_y"],
-                            table.loc[i, "Cartn_z"]],
-                           dtype=float) for i in table.index]
-    else:
-        return ValueError("Pandas DataFrame is not valid...")
-    return coords
 
--- tests/test_structures.py
+++ tests/test_structures.py
@@ -14,9 +14,7 @@ except ImportError:
 from proteofav.structures import mmCIF
 
 from prointvar.config import config
-from prointvar.structures import (parse_mmcif_categories_from_file,
-                                  get_contact_indexes_from_table, add_mmcif_contacts,
-                                  add_mmcif_new_pro_ids, get_coordinates)
+from prointvar.structures import add_mmcif_new_pro_ids
 
 
 @patch("prointvar.config.config", config)
@@ -32,11 +30,7 @@ class TestStructures(unittest.TestCase):
         self.inputbiocif = os.path.join(os.path.dirname(__file__), "testdata",
                                         "mmcif", "{}_bio.cif".format(self.pdbid))
 
-        self.parser_categories = parse_mmcif_categories_from_file
-        self.get_contacts = get_contact_indexes_from_table
-        self.add_contacts = add_mmcif_contacts
         self.add_mmcif_new_pro_ids = add_mmcif_new_pro_ids
-        self.get_coordinates = get_coordinates
 
         logging.disable(logging.DEBUG)
 
@@ -47,51 +41,9 @@ class TestStructures(unittest.TestCase):
         self.inputcif = None
         self.inputbiocif = None
 
-        self.parser_categories = None
-        self.get_contacts = None
-        self.add_contacts = None
         self.add_mmcif_new_pro_ids = None
-        self.get_coordinates = None
 
         logging.disable(logging.NOTSET)
-
-    def test_parser_categories_cif_data(self):
-        data = self.parser_categories(self.inputcif)
-        # CATEGORIES
-        self.assertEqual(data['_entry']['id'], [self.pdbid.upper()])
-        self.assertEqual(data['_cell']['entry_id'], [self.pdbid.upper()])
-        self.assertEqual(data['_symmetry']['entry_id'], [self.pdbid.upper()])
-
-    def test_parser_categories_selected_cif_data(self):
-        # CATEGORIES
-        data = self.parser_categories(self.inputcif, category='_entry')
-        self.assertEqual(data['_entry']['id'], [self.pdbid.upper()])
-        data = self.parser_categories(self.inputcif, category='_cell')
-        self.assertEqual(data['_cell']['entry_id'], [self.pdbid.upper()])
-        data = self.parser_categories(self.inputcif, category='_symmetry')
-        self.assertEqual(data['_symmetry']['entry_id'], [self.pdbid.upper()])
-
-    def test_parser_categories_excluded_cif_data(self):
-        # CATEGORIES
-        data = self.parser_categories(self.inputcif, excluded=('_atom_site',))
-        keys = [k for k in data.keys()]
-        self.assertNotIn('_atom_site', keys)
-
-    def test_get_contact_indexes(self):
-        data = mmCIF.read(self.inputcif)
-        indexes = self.get_contacts(data)
-        self.assertEqual(indexes[0], [0, 1, 2, 3, 4, 5, 6, 7, 8, 13])
-
-    def test_reader_add_contacts(self):
-        data = mmCIF.read(self.inputcif)
-        data = self.add_contacts(data)
-        keys = [k for k in data]
-        self.assertIn('contact_indexes', keys)
-
-    def test_add_contacts_value(self):
-        data = mmCIF.read(self.inputcif)
-        data = self.add_contacts(data)
-        self.assertEqual(data.loc[0, 'contact_indexes'], '0,1,2,3,4,5,6,7,8,13')
 
     def test_add_new_pro_id(self):
         data = mmCIF.read(self.inputbiocif)
@@ -106,11 +58,6 @@ class TestStructures(unittest.TestCase):
         self.assertIn("new_seq_id", keys)
         self.assertListEqual([k for k in data.new_asym_id.unique()], ['A'])
         self.assertEqual(1306, len([k for k in data.new_seq_id.unique()]))
-
-    def test_get_coordinates(self):
-        data = mmCIF.read(self.inputcif)
-        coords = get_coordinates(data)
-        self.assertEqual(coords[0].tolist(), [-7.069, 21.943, 18.77])
 
 
 if __name__ == '__main__':
